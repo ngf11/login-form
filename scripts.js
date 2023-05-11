ee7244a9d1fce4d55e17654004027515e78824f1
Nicolas Fuentes <nfuentes11@gmail.com>
2023-05-10 23:57:10 -0400

    form validation

--- scripts.js
+++ scripts.js
@@ -1,31 +1,38 @@
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("password2");
-const emailAlert = document.getElementById("message");
-const name = document.getElementById("firstName");
+const alert = document.getElementById("message");
+const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
 const phone = document.getElementById("tel");
 const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
   let messages = [];
-  e.preventDefault();
+  if (firstName.value === "" || firstName.value == null) {
+    messages.push("Name is required");
+  }
+  if (messages.length > 0) {
+    e.preventDefault();
+    alert.innerText = messages.join(", ");
+  }
+  console.log(messages);
 });
 
-function emailValidation() {
-  //   if (password.value === "" && confirmPassword.value === "") {
-  //     emailAlert.innerText = "";
-  //   }
+// function emailValidation() {
+//   //   if (password.value === "" && confirmPassword.value === "") {
+//   //     emailAlert.innerText = "";
+//   //   }
 
-  if (password.value === confirmPassword.value) {
-    emailAlert.style.color = "green";
-    emailAlert.innerText = "passwords matching";
-  }
+//   if (password.value === confirmPassword.value) {
+//     emailAlert.style.color = "green";
+//     emailAlert.innerText = "passwords matching";
+//   }
 
-  if (password.value != confirmPassword.value) {
-    emailAlert.style.border = " 5px solid rgba(233, 13, 13, 0.936)";
-    emailAlert.style.color = "red";
-    emailAlert.innerText = "passwords  do not matching";
-  }
-}
-emailValidation();
+//   if (password.value != confirmPassword.value) {
+//     emailAlert.style.border = " 5px solid rgba(233, 13, 13, 0.936)";
+//     emailAlert.style.color = "red";
+//     emailAlert.innerText = "passwords  do not matching";
+//   }
+// }
+// emailValidation();
